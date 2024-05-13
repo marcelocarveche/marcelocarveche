1e015cee893cccde7de3f6e19e27c0b849a4db39
Marcelo Carveche <marcelocarveche@gmail.com>
2024-05-13 15:30:31 -0400

    chore: config new tabsize

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <h1>Welcome to my personal page!</h1>
-    </React.StrictMode>
+   <React.StrictMode>
+      <h1>Welcome to my personal page!</h1>
+   </React.StrictMode>
 )
